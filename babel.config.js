32f8f4c5e7c613c698d6799415412e153e124150
Marcos Gomes <marcosgomesdev@gmail.com>
2023-12-02 16:03:51 -0300

    initial configutation

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,24 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: '.',
+        alias: {
+          '@hooks': './src/hooks',
+          '@utils': './src/utils',
+          '@types': './src/types',
+          '@routes': './src/routes',
+          '@styles': './src/styles',
+          '@assets': './src/assets',
+          '@themes': './src/themes',
+          '@screens': './src/screens',
+          '@services': './src/services',
+          '@components': './src/components',
+          '@interfaces': './src/interfaces',
+        },
+      },
+    ],
+  ],
 };
