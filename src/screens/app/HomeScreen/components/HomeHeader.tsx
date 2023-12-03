970c4b89130ef4aede10cd09a535d6066114a7ee
Marcos Gomes <marcosgomesdev@gmail.com>
2023-12-03 01:29:29 -0300

    create header home component

---
+++ src/screens/app/HomeScreen/components/HomeHeader.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Box, BoxProps, Text } from '@components';
+
+export function HomeHeader() {
+  return (
+    <Box {...$wrapper}>
+      <Box>
+        <Text preset="paragraphLarge" color="gray1">
+          Bem vindo(a) a
+        </Text>
+        <Text preset="headingMedium" bold color="grayBlack" mt="s10">
+          FAMILIA COSTA
+        </Text>
+      </Box>
+    </Box>
+  );
+}
+
+const $wrapper: BoxProps = {
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  paddingBottom: 's24',
+};
