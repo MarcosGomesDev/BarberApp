7a690ae9184e478840dc8987be335aae33ad3d4b
Marcos Gomes <marcosgomesdev@gmail.com>
2024-02-25 02:24:10 -0300

    create utils functions

---
+++ src/utils/date.ts
@@ -0,0 +1,57 @@
+import { format, utcToZonedTime } from 'date-fns-tz';
+import ptBR from 'date-fns/locale/pt-BR';
+import { capitalizeFirstLetter } from './capitalize';
+
+export function getCurrentDayName(): string {
+  const currentDate = new Date();
+  const currentDateBrazil = utcToZonedTime(currentDate, 'America/Sao_Paulo');
+
+  return capitalizeFirstLetter(
+    format(currentDateBrazil, 'EEEE', {
+      locale: ptBR,
+    }),
+  );
+}
+
+export function getCurrentMonthName(date: Date): string {
+  const currentDateBrazil = utcToZonedTime(date, 'America/Sao_Paulo');
+
+  return capitalizeFirstLetter(
+    format(currentDateBrazil, 'MMMM', {
+      locale: ptBR,
+    }),
+  );
+}
+
+export function getOnlyDay(date: Date): string {
+  const currentDateBrazil = utcToZonedTime(date, 'America/Sao_Paulo');
+
+  return format(currentDateBrazil, 'dd', {
+    locale: ptBR,
+  });
+}
+
+export function getOnlyHour(date: Date): string {
+  const currentDateBrazil = utcToZonedTime(date, 'America/Sao_Paulo');
+
+  return format(currentDateBrazil, 'HH:mm', {
+    locale: ptBR,
+  });
+}
+
+export function getCUrrentDate(): string {
+  const currentDate = new Date();
+  const currentDateBrazil = utcToZonedTime(currentDate, 'America/Sao_Paulo');
+
+  const monthName = format(currentDateBrazil, 'MMMM', {
+    locale: ptBR,
+  });
+
+  const capitalizedMonthName = capitalizeFirstLetter(monthName);
+
+  const day = format(currentDateBrazil, 'dd', {
+    locale: ptBR,
+  });
+
+  return `${day} de ${capitalizedMonthName}`;
+}
