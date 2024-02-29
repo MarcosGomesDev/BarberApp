0917d5acd4bab3eb43b4a6db498a8aab9a3e23a1
Marcos Gomes <marcosgomesdev@gmail.com>
2024-02-29 00:02:15 -0300

    create MenuServiceItemCard component

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,6 +4,7 @@ export * from './Box';
 export * from './Button';
 export * from './Form';
 export * from './Icon';
+export * from './MenuServiceItemCard';
 export * from './PasswordInput';
 export * from './ScheduleCard';
 export * from './Screen';
