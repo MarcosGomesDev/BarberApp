32fed61a730eec7df3bfbe9a7d6bfa03d390f6e8
Marcos Gomes <marcosgomesdev@gmail.com>
2024-02-29 00:02:59 -0300

    add new props on Screen component

--- src/components/Screen/index.tsx
+++ src/components/Screen/index.tsx
@@ -3,28 +3,37 @@ import {
   Appearance,
   KeyboardAvoidingView,
   Platform,
+  ScrollViewProps,
   StatusBar,
 } from 'react-native';
 
 import { Box, BoxProps } from '@components';
 import { useAppSafeArea, useAppTheme } from '@hooks';
 
-import { ScrollViewContainer, ViewContainer, ScreenHeader } from './components';
+import { ScreenHeader, ScrollViewContainer, ViewContainer } from './components';
 
-export interface ScreenProps extends BoxProps {
+export interface ScreenProps
+  extends BoxProps,
+    Omit<ScrollViewProps, 'children'> {
   children: React.ReactNode;
   HeaderComponent?: React.ReactNode;
   canGoBack?: boolean;
   isHome?: boolean;
   scrollable?: boolean;
+  hasHeader?: boolean;
   title?: string;
+  statusBarTranslucent?: boolean;
+  scrollPointShowBackgroundStatusBar?: number;
 }
 
 export function Screen({
   children,
   canGoBack = false,
   isHome = false,
-  scrollable = false,
+  statusBarTranslucent = false,
+  scrollPointShowBackgroundStatusBar = 100,
+  hasHeader = false,
+  scrollable = true,
   style,
   title,
   HeaderComponent,
@@ -33,29 +42,62 @@ export function Screen({
   const { bottom, top } = useAppSafeArea();
   const { colors } = useAppTheme();
 
+  const [scrolled, setScrolled] = React.useState(false);
+
   const colorSchema = Appearance.getColorScheme();
 
   const Container = scrollable ? ScrollViewContainer : ViewContainer;
+
+  function handleScroll(event: any) {
+    const scrollPosition = event.nativeEvent.contentOffset.y;
+    const threshold = scrollPointShowBackgroundStatusBar;
+
+    if (scrollPosition > threshold) {
+      setScrolled(true);
+    } else if (scrollPosition < threshold) {
+      setScrolled(false);
+    }
+  }
+
   return (
     <>
       <StatusBar
-        barStyle={colorSchema === 'dark' ? 'light-content' : 'dark-content'}
-        backgroundColor={colors.background}
+        barStyle={
+          statusBarTranslucent || colorSchema === 'dark'
+            ? 'light-content'
+            : 'dark-content'
+        }
+        animated
+        showHideTransition={'fade'}
+        backgroundColor={scrolled ? colors.background : 'transparent'}
+        translucent={statusBarTranslucent}
       />
       <KeyboardAvoidingView
         style={{ flex: 1 }}
         behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
-        <Box
-          style={{ backgroundColor: colors.background, paddingTop: top }}
-          alignContent="center">
-          <ScreenHeader
-            HeaderComponent={HeaderComponent}
-            canGoBack={canGoBack}
-            isHome={isHome}
-            title={title}
-          />
-        </Box>
-        <Container backgroundColor={colors.background}>
+        {hasHeader && (
+          <Box
+            style={{ backgroundColor: colors.background, paddingTop: top }}
+            alignContent="center">
+            <ScreenHeader
+              HeaderComponent={HeaderComponent}
+              canGoBack={canGoBack}
+              isHome={isHome}
+              title={title}
+            />
+          </Box>
+        )}
+        <Container
+          backgroundColor={colors.background}
+          {...boxProps}
+          onScroll={
+            scrollable
+              ? e => {
+                  handleScroll(e);
+                  boxProps.onScroll && boxProps.onScroll(e);
+                }
+              : undefined
+          }>
           <Box
             paddingHorizontal="s16"
             style={[{ paddingTop: top, paddingBottom: bottom }, style]}
