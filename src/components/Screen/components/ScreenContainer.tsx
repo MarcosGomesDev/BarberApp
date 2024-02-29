4ab5889f8d5ece3a67bdd0b809b18ebcdfdf094a
Marcos Gomes <marcosgomesdev@gmail.com>
2024-02-29 00:03:43 -0300

    add new props on ScreenContainer

--- src/components/Screen/components/ScreenContainer.tsx
+++ src/components/Screen/components/ScreenContainer.tsx
@@ -2,19 +2,26 @@ import React from 'react';
 import {
   Keyboard,
   ScrollView,
+  ScrollViewProps,
   TouchableWithoutFeedback,
   View,
 } from 'react-native';
 
-interface Props {
+interface Props extends Omit<ScrollViewProps, 'children'> {
   children: React.ReactNode;
   backgroundColor: string;
 }
-export function ScrollViewContainer({ children, backgroundColor }: Props) {
+
+export function ScrollViewContainer({
+  children,
+  backgroundColor,
+  ...props
+}: Props) {
   return (
     <ScrollView
       keyboardShouldPersistTaps="handled"
       showsVerticalScrollIndicator={false}
+      {...props}
       style={{ backgroundColor, flex: 1 }}>
       {children}
     </ScrollView>
